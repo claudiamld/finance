ab3e9c7ca017542b62072b4be264fc1c181bcf8c
Claudia Dias <claudiamld@outlook.com>
2022-07-04 18:50:21 -0300

    Dashboard page created

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,8 +1,56 @@
+import Form from "../../components/Form"
+import CardsList from '../../components/CardsList'
+import EmptyList from '../../assets/NoCard.png'
+import { CardsListContent, Container, Content, TransactionNavigation } from "./styles"
+import { Button } from "../../components/Button"
+import { useHistory } from "react-router-dom"
+import { useContext } from "react"
+import { TransactionsContext } from "../../providers/Transactions/transactions"
+
 const Dashboard = () => {
+    const {transactions, total} = useContext(TransactionsContext)
+
+    const history = useHistory()
+
+    const handleNavigation = (path) => {
+        return history.push(path)
+    }
+
     return(
-        <div>
-            Dashboard
-        </div>
+        <Container>
+            <header>
+                <h1>Finance</h1>
+                <Button greySchema onClick={() => handleNavigation("/")}>Início</Button>
+            </header>
+            <Content>
+              <div>
+                <Form />
+                <div className="balance">
+                  <div>
+                    <h3>Valor total:</h3>
+                    <span>O valor se refere ao saldo</span>
+                  </div>
+                  <h4>R$ {total},00</h4>
+                </div>
+              </div>
+                <CardsListContent>
+                    <TransactionNavigation>
+                      <h3>Resumo financeiro</h3>
+                      <div>
+                        <Button>Todos</Button>
+                        <Button>Entradas</Button>
+                        <Button>Despesas</Button>
+                      </div>
+                    </TransactionNavigation>
+                    {transactions.length > 0 ? <CardsList /> : (
+                      <div className="emptyList">
+                        <h2>Você ainda não possui nenhum lançamento</h2>
+                        <img src={EmptyList} alt="empty-list" />
+                      </div>
+                    )}
+                </CardsListContent>
+            </Content>
+        </Container>
     )
 }
 export default Dashboard
---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,80 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    max-width: 100vw;
+
+    header{
+        display: flex;
+        justify-content: space-around;
+        width: 100%;
+        color: #219ebc;
+        border-bottom: 1px solid #c5c5c5;
+        box-shadow: 5px 0px 15px 1px rgba(0,0,0,0.52);
+        padding: 20px 0px 20px 0px;
+
+        h1{
+            margin-right: 45rem;
+        }
+    }
+`
+
+export const Content = styled.div`
+    width: 80%;
+    display: flex;
+    justify-content: space-between;
+    margin-top: 3rem;
+
+    .balance{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        margin-top: 1rem;
+        padding: 20px 0px;
+        border: 1px solid #E9ECEF;
+        border-radius: 4px;
+
+        h3{
+            color: #023047;
+            margin-bottom: 5px;
+        }
+
+        h4{
+            font-size: 1.3rem;
+            color: #219ebc;
+        }
+        span{
+            color: #868E96;
+        }
+    }
+`
+
+export const CardsListContent = styled.div`
+    width: 55%;
+    .emptyList{
+        h2{
+            margin-bottom: 20px;
+            color: #555b6e;
+        }
+    }
+`
+
+export const TransactionNavigation = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 40px;
+
+    h3{
+        color: #023047;
+    }
+
+    div{
+        width: 40%;
+        display: flex;
+        justify-content: space-between;
+    }
+`
